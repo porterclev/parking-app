bd8874b4ea30235fc259c81b876e1a541638ea68
Alkon0st <151362849+Alkon0st@users.noreply.github.com>
2025-03-27 19:41:41 -0700

    Update db.js

--- PickNPark/backend/db/db.js
+++ PickNPark/backend/db/db.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.DB_URI);
+        await mongoose.connect(process.env.DB_URI); // create a .env file in the root directory of the project
         console.log('MongoDB connected!');
     } catch(error) {
         console.log('MongoDB connection failed!');
