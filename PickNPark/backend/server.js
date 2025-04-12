0581662fb086fd3b286ca7be2cff655dba9b649e
Mmr57190 <flavien.maameri@epitech.eu>
2025-04-12 07:02:38 +0200

    add reservation backend

--- PickNPark/backend/server.js
+++ PickNPark/backend/server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express');
 const connectDB = require('./db/db');
 const authRoutes = require('./routes/authRoutes');
+const parkingRoutes = require('./routes/parkingRoutes');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const cors = require('cors');
@@ -19,6 +20,8 @@ app.get('/', (req, res) => {
 
 // Routes
 app.use('/api/auth', authRoutes);
+app.use('/api/parking', parkingRoutes);
+
 
 app.listen(PORT, () => {
   console.log(`Server is up and running at http://localhost:${PORT} 🚀`);
