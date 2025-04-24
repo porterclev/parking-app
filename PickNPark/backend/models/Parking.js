105e806d5ccd9553c7827e97150de6c0edf86257
Mmr57190 <flavien.maameri@epitech.eu>
2025-04-25 00:34:07 +0200

    add owner in models, controller and route

--- PickNPark/backend/models/Parking.js
+++ PickNPark/backend/models/Parking.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const ParkingSchema = new mongoose.Schema({
   spotNumber:  { type: String, required: true },
   location:    { type: String, default: '' },
+  owner:        { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null },
   reservedBy:  { type: mongoose.Schema.Types.ObjectId, ref: 'User', default: null },
   reservedAt:  { type: Date, default: null },
   reservedUntil:{ type: Date, default: null }
